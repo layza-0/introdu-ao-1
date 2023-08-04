fd4fb1a16fe31f3f2b0bfe3b89800461182feb21
layza-0 <132698296+layza-0@users.noreply.github.com>
2023-08-04 17:21:26 +0000

    É só ler

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from "express";      // Requisição do pacote do express
+import dotenv from "dotenv";
+
+import { selectUsuarios } from "./bd.js";
+
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
+
+dotenv.config();
 
 app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   res.json({
@@ -9,16 +15,6 @@ app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   console.log("Rota / solicitada");
 });
 
-app.listen(port, () => {            // Um socket para "escutar" as requisições
-  console.log(`Serviço escutando na porta:  ${port}`);
-});
-
-//index.js
-import dotenv from "dotenv";
-
-dotenv.config();
-
-import { selectUsuarios } from "./bd.js";
 
 app.get("/usuarios", async (req, res) => {
   console.log("Rota GET/usuarios solicitada");
@@ -28,4 +24,8 @@ app.get("/usuarios", async (req, res) => {
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
+});
+
+app.listen(port, () => {            // Um socket para "escutar" as requisições
+  console.log(`Serviço escutando na porta:  ${port}`);
 });
